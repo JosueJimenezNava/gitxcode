2076ffac28c646a4009cbbbfc6fa419970417115
Josue Jimenez <josue.sistemas@gmail.com>
2021-10-15 09:44:17 -0600

    mi primer commit

---
+++ gitxcode/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  gitxcode
+//
+//  Created by Josue Jimenez Nava on 15/10/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
